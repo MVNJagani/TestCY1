14b7c51915638477782b98504cd9f59a020e8424
ved1007 <ved1007@gmail.com>
2021-08-19 10:04:22 -0500

    report code BDD added

--- cypress/cucumber-html-report.js
+++ cypress/cucumber-html-report.js
@@ -18,15 +18,10 @@ var options = {
         metadata: {
 
             "App Version": "0.3.2",
-
             "Test Environment": "STAGING",
-
             "Browser": "Chrome  54.0.2840.98",
-
             "Platform": "Windows 10",
-
             "Parallel": "Scenarios",
-
             "Executed": "Remote"
 
         }
